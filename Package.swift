fa5120cda9a42be83af94d55caca1a6f5f55855f
adamz <adam@zethrae.us>
2025-04-01 11:22:09 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "AsyncBroadcaster",
+  name: "swift-async-broadcaster",
   platforms: [.iOS(.v18), .macOS(.v15), .tvOS(.v18), .watchOS(.v11), .visionOS(.v2)],
   products: [
     .library(
